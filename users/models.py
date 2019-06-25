5e4ea6eec87f05b2af0882fed4adee1d552188da
Madhangog <madhanmanoj2017@gmail.com>
2019-06-25 14:24:39 +0530

    updated something

--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+
+
 
 # Create your models here.
 
@@ -10,6 +13,9 @@ class Audience(models.Model):
 	username = models.CharField(max_length=50)
 	password = models.CharField(max_length=30)
 	mobile = models.CharField(max_length=12)
+	date_joined = models.DateTimeField(default=timezone.now)
+	is_active = models.BooleanField(default=False)
+
 	
 	def __str__(self):
 		return self.username
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
     # url(r'^$', views.index, name = 'user'),
     url(r'^register/', views.UserList.as_view(), name = 'register')
+
 ]
